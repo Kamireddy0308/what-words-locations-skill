0236eee6f181a4f2722e4e2c34bb565c588e90c5
Kamireddy Viswa Teja <75202740+Kamireddy0308@users.noreply.github.com>
2020-12-01 20:03:57 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,19 @@
 from mycroft import MycroftSkill, intent_file_handler
+import requests
+import json
+from mycroft.utils.log import getLogger
 
+LOGGER = getLogger(_name_)
+def what_three_words():
+    x = 'bricht'
+    y = 'schach'
+    z = 'bauen'
+    value = requests.get(
+        'https://api.what3words.com/v3/convert-to-coordinates?words=' + x + '.' + y + '.' + z + '&key=4WBPDAOJ',
+        verify=False)
+    json_value = value.json()
+    response = json_value['nearestPlace']
+    return response
 
 class WhatWordsLocations(MycroftSkill):
     def __init__(self):
@@ -7,7 +21,10 @@ class WhatWordsLocations(MycroftSkill):
 
     @intent_file_handler('locations.words.what.intent')
     def handle_locations_words_what(self, message):
-        self.speak_dialog('locations.words.what')
+        output = what_three_words()
+        LOGGER.debug(message)
+        self.speak_dialog(output)
+        #self.speak_dialog('locations.words.what')
 
 
 def create_skill():
