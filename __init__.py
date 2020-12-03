f84f4d6e358473d625f342b0b9918488e138c016
Kamireddy Viswa Teja <75202740+Kamireddy0308@users.noreply.github.com>
2020-12-03 19:01:59 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,32 +1,27 @@
 from mycroft import MycroftSkill, intent_file_handler
 import requests
 import json
-from mycroft.utils.log import getLogger
+from mycroft.util.log import LOG
 
-LOGGER = getLogger(_name_)
-def what_three_words():
-    x = 'task'
-    y = 'exile'
-    z = 'sorround'
-    value = requests.get(
-        'https://api.what3words.com/v3/convert-to-coordinates?words=' + x + '.' + y + '.' + z + '&key=4WBPDAOJ',
-        verify=False)
+def what_three_words(words):
+    url =
+    print(url)
+    value = requests.get(url)
     json_value = value.json()
     response = json_value['nearestPlace']
     return response
 
 class WhatWordsLocations(MycroftSkill):
-    def __init__(self):
-        MycroftSkill.__init__(self)
+    def __int__(self):
+        MycroftSkill.__init(self)
 
-    @intent_file_handler('locations.words.what.intent')
-    def handle_locations_words_what(self, message):
-        output = what_three_words()
-        LOGGER.debug(message)
-        self.speak_dialog(output)
-        #self.speak_dialog('locations.words.what')
-
-
+        @intent_file_handler('locations.words.what.intent')
+        def handle_locations_words_what(self, message):
+            getwordlist = message.data["wordlist"]
+            splitwords = getwordlist.split(" ")
+            joinwordformat = ".".join(splitwords)
+            output = what_three_words(joinwordformat)
+            self.speak(output)
 def create_skill():
     return WhatWordsLocations()
 
