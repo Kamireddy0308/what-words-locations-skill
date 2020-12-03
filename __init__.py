3414dab5380e77560d67be37d1aed5e6491a341e
Kamireddy Viswa Teja <75202740+Kamireddy0308@users.noreply.github.com>
2020-12-03 19:02:43 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,25 +3,28 @@ import requests
 import json
 from mycroft.util.log import LOG
 
-def what_three_words(words):
-    url =
-    print(url)
-    value = requests.get(url)
-    json_value = value.json()
-    response = json_value['nearestPlace']
-    return response
-
 class WhatWordsLocations(MycroftSkill):
     def __int__(self):
-        MycroftSkill.__init(self)
+        MycroftSkill.__init__(self)
 
-        @intent_file_handler('locations.words.what.intent')
-        def handle_locations_words_what(self, message):
-            getwordlist = message.data["wordlist"]
-            splitwords = getwordlist.split(" ")
-            joinwordformat = ".".join(splitwords)
-            output = what_three_words(joinwordformat)
-            self.speak(output)
+    @intent_file_handler('locations.words.what.intent')
+    def handle_locations_words_what(self, message):
+        getwordlist = message.data["wordlist"]
+        splitwords = getwordlist.split(" ")
+        joinwordformat = ".".join(splitwords)
+        output = self.what_three_words(joinwordformat)
+
+    def what_three_words(self, words):
+        url ='https://api.what3words.com/v3/convert-to-coordinates?words={0}&key=4WBPDAOJ'.format(words)
+        print(url)
+        value = requests.get(url)
+        json_value = value.json()
+        try:
+            response = json_value['nearestPlace']
+            self.speak(response)
+        except:
+            self.speak("I didn't find any coordinates matching" + wordlist)
+
 def create_skill():
     return WhatWordsLocations()
 
