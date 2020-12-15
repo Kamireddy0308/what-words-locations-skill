4a796121df416186ac0f20ec67d618aee18ecb4e
Kamireddy Viswa Teja <75202740+Kamireddy0308@users.noreply.github.com>
2020-12-15 14:22:11 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -29,7 +29,7 @@ class WhatWordsLocations(MycroftSkill):
             self.speak(response)
             response = json_value['words']
             self.speak(response)
-            response = str(json_value['coordinates']['lng'])
+            response = str(json_value['coordinates']['lng'],['lat'])
             self.speak(response)
             
         except:
